db76dbb2d3ba2cb6b18e5a072b6a1c79b969f8fe
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2024-01-03 11:45:21 -0800

    e

---
+++ DimaKarpenko.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class KarpenkoDima here.
+ *
+ * @author D1teri or Dima K.
+ * @version 01/03/24
+ */
+public class DimaKarpenko extends Animal
+{
+    public DimaKarpenko() {
+        super("Dima Karpenko", "white", 13);
+    }
+  
+    public String getType() {
+        return "BoopleSnoot";
+    }
+}
